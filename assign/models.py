75489495a83f7a0edd10b4485c7fefd5efc56759
ShotaroKawano <shotaro.kawano009@gmail.com>
2021-01-06 05:17:25 +0900

    Add: サンプルコードとカスタムユーザーの追加

--- assign/models.py
+++ assign/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class User(AbstractUser):
+    pass
+
+
+class BoardModel(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    author = models.CharField(max_length=100)
+    # images = models.ImageField(upload_to='')
+    good = models.IntegerField()
+    read = models.IntegerField()
+    readtext = models.CharField(max_length=200)
--- assign/views.py
+++ assign/views.py
@@ -1,3 +1,48 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from .models import BoardModel
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+
+def signupfunc(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        try:
+            User.objects.get(username=username)
+            return render(request, 'signup.html', {'error': 'このユーザーは登録されています。'})
+        except:
+            user = User.objects.create_user(username, '', password)
+            return render(request, 'signup.html', {'some': 100})
+    return render(request, 'signup.html')
+
+
+def loginfunc(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('list')
+        else:
+            return redirect('login')
+    return render(request, 'login.html')
+
+
+@login_required
+def listfunc(request):
+    object_list = BoardModel.objects.all()
+    return render(request, 'list.html', {'object_list': object_list})
+
+
+def logoutfunc(request):
+    logout(request)
+    return redirect('login')
+
+
+def detailfunc(request, pk):
+    object = BoardModel.objects.get(pk=pk)
+    return render(request, 'detail.html', {'object': object})
