c681801726632ecec0a2669ba6acc8ccb1f30680
ShotaroKawano <shotaro.kawano009@gmail.com>
2021-01-06 12:40:46 +0900

    Add: 管理画面にmodelを登録

--- assign/admin.py
+++ assign/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import Notification
+from .models import Notification, ProjectPhase, Project, ProjectMember, MonthlyWorkingTime, DailyWorkingTime
 
 # Register your models here.
 admin.site.register(Notification)
+admin.site.register(ProjectPhase)
+admin.site.register(Project)
+admin.site.register(ProjectMember)
+admin.site.register(MonthlyWorkingTime)
+admin.site.register(DailyWorkingTime)
