2e1983cb83a58fda906597466724c849448c7a0c
ShotaroKawano <shotaro.kawano009@gmail.com>
2021-01-06 12:57:52 +0900

    Add: working_time系のテーブルにProjectMemberの外部キーを追加

--- assign/models.py
+++ assign/models.py
@@ -9,9 +9,16 @@ class Notification(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return '[Alert]' + self.content
+
+
 
 class ProjectPhase(models.Model):
     phase = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.phase
 
 
 class Project(models.Model):
@@ -26,6 +33,9 @@ class Project(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return self.name + '[' + self.start_date + '-' + self.end_date + ']'
+
 
 class ProjectMember(models.Model):
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
@@ -33,18 +43,29 @@ class ProjectMember(models.Model):
     is_management = models.BooleanField()
     created_at = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return self.project + self.user
+
 
 class MonthlyWorkingTime(models.Model):
+    project_member = models.ForeignKey(ProjectMember, default=0, on_delete=models.CASCADE)
     target_month = models.CharField(max_length=10)
     planed_working_time = models.IntegerField()
     actual_working_time = models.IntegerField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return self.project_member + self.target_month
+
 
 class DailyWorkingTime(models.Model):
+    project_member = models.ForeignKey(ProjectMember, default=0, on_delete=models.CASCADE)
     target_day = models.CharField(max_length=10)
     target_month = models.CharField(max_length=10)
     working_time = models.IntegerField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.project_member + self.target_month + self.target_day
