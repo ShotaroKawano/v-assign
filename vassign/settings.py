759e77c122c051a948bb630b4db89a937e3ac067
ShotaroKawano <shotaro.kawano009@gmail.com>
2021-01-07 13:40:04 +0900

    Fix: ログを追加

--- vassign/settings.py
+++ vassign/settings.py
@@ -140,3 +140,20 @@ if not DEBUG:
 # if DEBUG:
     import django_heroku
     django_heroku.settings(locals())
+
+import logging
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': os.getenv('DJANGO_LOG_LEVEL', 'DEBUG'),
+        },
+    },
+}
