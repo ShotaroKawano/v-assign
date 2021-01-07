6b0c450cdd7f659b7dc52f5548c0ed0141c44110
ShotaroKawano <shotaro.kawano009@gmail.com>
2021-01-07 13:58:48 +0900

    Add: collectstaticを実行

---
+++ staticfiles/project_resource.js
@@ -0,0 +1,14 @@
+$(function() {
+  var $input = $('.monthly_working_time')
+  var $updated = $('#updated_list')
+  updated = []
+  $input.on('input', function(event) {
+    console.log(this.name);
+    if (!updated.includes(this.name)) {
+      updated.push(this.name)
+    }
+    console.log(updated);
+    $updated.val(updated)
+  });
+
+});
